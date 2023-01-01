09ae2c5eb623ec6fd81e63de78772d5490628c78
Stewart Bennell <stewart@bennell.me>
2023-01-02 10:28:06 +1100

    Update PBSDiscordDark.sh

--- PBSDiscordDark.sh
+++ PBSDiscordDark.sh
@@ -37,7 +37,7 @@ hash sed 2>/dev/null || {
     exit 1;
 }
 
-hash PBSVersion 2>/dev/null || { 
+hash proxmox-backup-client version 2>/dev/null || { 
     echo -e >&2 "${BRED}PVE installation required but missing from your system${REG}";
     exit 1;
 }
